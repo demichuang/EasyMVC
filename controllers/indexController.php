cedf9fb1d37df8f77e02efcc03de76f31f9894c9
hi <here>
2016-07-31 08:20:12 +0000

    tidy index program

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -2,93 +2,69 @@
 
 class indexController extends Controller {
     
-    
+    //  到首頁(登入頁)
     function index(){
         $this->view("index");
-    }       //到首頁(登入頁)
+    }   
 
-    function gosignup() {
-        $this->view("index_sign");
-    }   //到註冊頁
-    
+    //  未登入前點其他連結
     function nologin(){
         $this->view("index",1); //回登入頁並顯示需先登入
-    }     //未登入前點其他連結
+    }     
     
+    //  點登入按鈕
     function login(){
-        $user=$_POST["txtUserName"];
-        $password=$_POST['txtPassword'];
+        $user=$_POST["txtUserName"];        //  得輸入的username
+        $password=$_POST['txtPassword'];    //  得輸入的userpassword
     
-    	$login=$this->model("sqlcommand");
-    	$row =$login->logincheck($user,$password);
-    
-      // 登入成功(如果輸入的username非空值，且user資料表內有一筆相符的資料)
-     	if (trim($user) !="" & $row == 1)       
+    	$row =$this->model("sqlcommand")->logincheck($user,$password);  // 取user資料表與輸入的username和userpassword相符的資料筆數
+
+     	if (trim($user) !="" & $row == 1)   // 登入成功(如果輸入的username非空值，且user資料表內有一筆相符的資料)    
     	{
     		$_SESSION["userName"]=$user;    // $_SESSION['userName']設為輸入的username
-    		$this->view("index");                  // 跳轉回到原頁面(index.php)
-    		                                      // 離開php程式
+    		$this->view("index");           // 回到登入頁	                                      // 離開php程式
     	}
-    //	登入失敗
-    	else                                    
+    	
+    	else                                //	登入失敗    
     	{
-    	  $_SESSION["userName"]="Guest";        // $_SESSION["userName"]設為"Guest"
-      	
-      	$this->view("index",2);   // 跳轉回到原頁面(index.php)，傳id=2值，顯示輸入錯誤或不是會員
-      	                             // 離開php程式
-    // 	}
-            
+    	    $_SESSION["userName"]="Guest";  // $_SESSION["userName"]設為"Guest"
+      	    $this->view("index",2);         // 回到登入頁，傳data=2值，顯示輸入錯誤或不是會員
+      	}
+    }       
+    
+    //  到註冊頁
+    function gosignup() {
+        $this->view("index_sign");
+    }    
+
+    //  點註冊按鈕
+    function signup(){
+        $newuser=$_POST["newtxtUserName"];      //  得輸入的新username
+        $newpassword=$_POST['newtxtPassword'];  //  得輸入的新userpassword
         
+    	$result =$this->model("sqlcommand")->signupcheck($newuser);   // 取user資料表與新輸入的username相符的資料
+    	$row = mysqli_fetch_array($result);                           // 取資料
         
+    	if (mysqli_num_rows($result)>0)         // 如果有與輸入的username相符的資料                        
+    	{
+    	  if($row['userpassword']==$newpassword)    // 如果輸入的userpassword也相符
+    	    $this->view("index",3);                 // 回到登入頁，傳data=3值，顯示本來就是會員了     
+                                        
+    	  else                                      // 如果輸入的userpassword不相符    
+    	    $this->view("index_sign",4);            // 回到註冊頁，傳data=4值，顯示帳號名已被使用
+    	}
+    	
+    	else                                    // 如果沒有與輸入的username相符的資料
+    	{
+        	$result =$this->model("sqlcommand")->adduser($newuser,$newpassword);    // 新增輸入的username和userpassword至user資料表
+            $this->view("index",5);                                                 // 回到登入頁，傳data=5值，顯示現在是會員了
+    	}
     }
     
-}       //點登入按鈕
-
-function logout(){
-    
-    $_SESSION["userName"]= "Guest";   // $_SESSION["userName"]設為"Guest"
-$this->view("index");   
-}
-
-
-function signup(){
-    // 檢查user資料表內是否有與輸入的username相符的資料
-    $newuser=$_POST["newtxtUserName"];
-    $newpassword=$_POST['newtxtPassword'];
-    
-    
-    $signup=$this->model("sqlcommand");
-	$result =$signup->signupcheck($newuser);
-    
-	$row = mysqli_fetch_array($result);
-  
-  // 如果有與輸入的username相符的資料
-	if (mysqli_num_rows($result)>0)                         
-	{
-	  // 如果輸入的userpassword也相符
-	  if($row['userpassword']==$newpassword)    
-	  {
-  		$this->view("index",3);                           
-	  }
-	  // 如果輸入的userpassword不相符
-	  else    
-	  {
-	    	$this->view("index_sign",4);                                     // 離開php程式
-	  }
-	}
-	// 如果沒有與輸入的username相符的資料
-	else    
-	{
-	  // 新增輸入的username和userpassword至user資料表
-   $newsignup=$this->model("sqlcommand");
-	$result =$newsignup->adduser($newuser,$newpassword);
-    
-    $this->view("index",5); 
-	}
-    
-    
-    
-}
-
+    //  點登出按鈕
+    function logout(){
+        $_SESSION["userName"]= "Guest";   // $_SESSION["userName"]設為"Guest"
+        $this->view("index");             // 回到登入頁
+    }
 }
 ?>
